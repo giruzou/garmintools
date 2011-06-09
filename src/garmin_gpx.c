cc847d6788e78ff15d037b77cf5f8b5644d3963f
Markus Braun <markus.braun@krawel.de>
2011-06-09 15:06:05 +0200

    updated patch for better GPX generation using Garmin extensions
    Description:
    http://code.google.com/p/garmintools/issues/detail?id=15
    
    Source:
    http://garmintools.googlecode.com/issues/attachment?aid=753452771107486670&name=gpx_laps_hr_cad-02.diff&token=a1ef98b67d9324d0c5b75eba14b8d318

--- src/garmin_gpx.c
+++ src/garmin_gpx.c
@@ -20,6 +20,7 @@
 #include "config.h"
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include "garmin.h"
 
@@ -46,144 +47,143 @@ get_gpx_data ( garmin_data *    fulldata,
                position_type *  sw,
                position_type *  ne )
 {
-	garmin_list *       dlist;
-	garmin_list_node *  node;
-	garmin_list_node *  lapnode;
-	garmin_data *       point;
-	D304 *              d304;
-	route_point *       rp;
-	float               minlat =   90.0;
-	float               maxlat =  -90.0;
-	float               minlon =  180.0;
-	float               maxlon = -180.0;
-	int                 ok     = 0;
-	garmin_data *		data;
-	garmin_list *     glaps;
-	garmin_data*     points;
+  garmin_list *       dlist;
+  garmin_list_node *  node;
+  garmin_list_node *  lapnode;
+  garmin_data *       point;
+  D304 *              d304;
+  route_point *       rp;
+  float               minlat =   90.0;
+  float               maxlat =  -90.0;
+  float               minlon =  180.0;
+  float               maxlon = -180.0;
+  int                 ok     = 0;
+  garmin_data *		data;
+  garmin_list *     glaps;
+  garmin_data*     points;
 
-	if ( fulldata != NULL ) {
+  if ( fulldata != NULL ) {
 
-	   // lists: 1=laps, 2=points
+    // lists: 1=laps, 2=points
 
-	   glaps=garmin_list_data(fulldata,1)->data; // get lap information
+    glaps=garmin_list_data(fulldata,1)->data; // get lap information
 
-	   // fixme: crash if we didn't get it
-	   int laps=glaps->elements+1;
+    // fixme: crash if we didn't get it
+    int laps=glaps->elements+1;
 
-	   *tracks = malloc(sizeof(route_point) * (laps));
-	   memset(*tracks,0,laps*sizeof(route_point));
+    *tracks = malloc(sizeof(route_point) * (laps));
+    memset(*tracks,0,laps*sizeof(route_point));
 
-	   int curlapnum=0;
+    int curlapnum=0;
+
+    lapnode=glaps->head;
+    D1015* lapdata=lapnode->data->data;
+
+    data = garmin_list_data(fulldata, 2); // get track points
+
+    if ( data == NULL ) {
+
+      printf("get_gpx_data: no track points found\n");
+
+    } else if ( data->type == data_Dlist ) {
+
+      dlist = data->data;
+
+      points = malloc(sizeof(route_point) * (dlist->elements+1+(laps*2)));
+      (*tracks)[curlapnum]=points;
+      rp = points;
+
+      int pause=0;
+
+      for ( node = dlist->head; node != NULL; node = node->next ) {
+	point = node->data;
+
+	switch (point->type) {
+	  case data_D304: // position point
+
+	    d304 = point->data;
+
+	    if ( d304->posn.lat == 0x7fffffff && d304->posn.lon == 0x7fffffff ) {
+	      pause++;
+	      continue;
+	    }
+
+	    rp->lap=0;
+	    if (lapdata!=NULL) {
+	      if (d304->time >= lapdata->start_time) {
+		if (curlapnum>0) {
+		  rp->t=0; // end previous lap
+		  rp++;
+		  (*tracks)[curlapnum]=rp; // new track
+		}
+		curlapnum++;
+
+		rp->lap=curlapnum;
+		if (d304->time != lapdata->start_time) {
+		  // if lap start point doesn't exist, create it
+		  rp->lat = SEMI2DEG(lapdata->begin.lat);
+		  rp->lon = SEMI2DEG(lapdata->begin.lon);
+
+		  rp->elev = d304->alt; // lap data doesn't contain alt :(
+		  rp->hr=d304->heart_rate;
+		  rp->cad=d304->cadence;
+
+		  rp->t = lapdata->start_time;
+		  rp++;
+		  rp->lap=0;
+		  rp->pause=0;
+		}
+		lapnode=lapnode->next;
+		if (lapnode!=NULL)
+		  lapdata=lapnode->data->data;
+		else
+		  lapdata=NULL; // last lap
+	      }
+	    }
+
+	    rp->lat = SEMI2DEG(d304->posn.lat);
+	    rp->lon = SEMI2DEG(d304->posn.lon);
+	    rp->elev = d304->alt;
+	    rp->t = d304->time;
+	    rp->hr=d304->heart_rate;
+	    rp->cad=d304->cadence;
 
 
-	   lapnode=glaps->head;
-	   D1015* lapdata=lapnode->data->data;
+	    if (pause==2) {
+	      rp->pause=1;
+	      pause=0;
+	    } else rp->pause=0;
 
-	   data = garmin_list_data(fulldata, 2); // get track points
+	    if ( rp->lat < minlat ) minlat = rp->lat;
+	    if ( rp->lat > maxlat ) maxlat = rp->lat;
+	    if ( rp->lon < minlon ) minlon = rp->lon;
+	    if ( rp->lon > maxlon ) maxlon = rp->lon;
 
-	   if ( data == NULL ) {
+	    ++rp;
+	    break;
+	  default: //printf("unknown frame %i\n", point->type);
+	    ;
+	}
+      }
+      rp->t = 0;
 
-		   printf("get_gpx_data: no track points found\n");
+      /* Now we can fill in the center coordinate and bounding box. */
 
-	   } else if ( data->type == data_Dlist ) {
+      ne->lat = DEG2SEMI(maxlat);
+      ne->lon = DEG2SEMI(maxlon);
 
-			dlist = data->data;
+      sw->lat = DEG2SEMI(minlat);
+      sw->lon = DEG2SEMI(minlon);
 
-			points = malloc(sizeof(route_point) * (dlist->elements+1+(laps*2)));
-			(*tracks)[curlapnum]=points;
-			rp = points;
+      ok = 1;
+    } else {
+      printf("get_gpx_data: invalid track point list\n");
+    }
+  } else {
+    printf("get_gpx_data: NULL data pointer\n");
+  }
 
-			int pause=0;
-
-			for ( node = dlist->head; node != NULL; node = node->next ) {
-				point = node->data;
-
-				switch (point->type) {
-				case data_D304: // position point
-
-					d304 = point->data;
-
-					if ( d304->posn.lat == 0x7fffffff && d304->posn.lon == 0x7fffffff ) {
-						pause++;
-						continue;
-					}
-
-					rp->lap=0;
-					if (lapdata!=NULL) {
-						if (d304->time >= lapdata->start_time) {
-							if (curlapnum>0) {
-								rp->t=0; // end previous lap
-								rp++;
-								(*tracks)[curlapnum]=rp; // new track
-							}
-							curlapnum++;
-
-							rp->lap=curlapnum;
-							if (d304->time != lapdata->start_time) {
-								// if lap start point doesn't exist, create it
-								rp->lat = SEMI2DEG(lapdata->begin.lat);
-								rp->lon = SEMI2DEG(lapdata->begin.lon);
-
-								rp->elev = d304->alt; // lap data doesn't contain alt :(
-								rp->hr=d304->heart_rate;
-								rp->cad=d304->cadence;
-
-								rp->t = lapdata->start_time;
-								rp++;
-								rp->lap=0;
-								rp->pause=0;
-							}
-							lapnode=lapnode->next;
-							if (lapnode!=NULL)
-								lapdata=lapnode->data->data;
-							else
-								lapdata=NULL; // last lap
-						}
-					}
-
-					rp->lat = SEMI2DEG(d304->posn.lat);
-					rp->lon = SEMI2DEG(d304->posn.lon);
-					rp->elev = d304->alt;
-					rp->t = d304->time;
-					rp->hr=d304->heart_rate;
-					rp->cad=d304->cadence;
-
-
-					if (pause==2) {
-						rp->pause=1;
-						pause=0;
-					} else rp->pause=0;
-
-					if ( rp->lat < minlat ) minlat = rp->lat;
-					if ( rp->lat > maxlat ) maxlat = rp->lat;
-					if ( rp->lon < minlon ) minlon = rp->lon;
-					if ( rp->lon > maxlon ) maxlon = rp->lon;
-
-					++rp;
-					break;
-				default: //printf("unknown frame %i\n", point->type);
-					;
-				}
-			}
-			rp->t = 0;
-
-			/* Now we can fill in the center coordinate and bounding box. */
-
-			ne->lat = DEG2SEMI(maxlat);
-			ne->lon = DEG2SEMI(maxlon);
-
-			sw->lat = DEG2SEMI(minlat);
-			sw->lon = DEG2SEMI(minlon);
-
-			ok = 1;
-			} else {
-				printf("get_gpx_data: invalid track point list\n");
-			}
-	  } else {
-		  printf("get_gpx_data: NULL data pointer\n");
-	  }
-
-	  return ok;
+  return ok;
 }
 
 
@@ -231,9 +231,12 @@ print_gpx_header ( FILE *               fp,
   fprintf(fp,"<gpx version=\"1.1\"\n"
     "creator=\"Garmin Forerunner Tools - http://garmintools.googlecode.com\"\n"
     "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
-	"xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"\n"
-    "xmlns=\"http://www.topografix.com/GPX/1/0\"\n"
-    "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/0/gpx.xsd\">\n");
+    "xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"\n"
+    "xmlns=\"http://www.topografix.com/GPX/1/1\"\n"
+    "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 "
+    "http://www.topografix.com/GPX/1/1/gpx.xsd "
+    "http://www.garmin.com/xmlschemas/TrackPointExtension/v1 "
+    "http://www8.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\">\n");
 }
 
 static void
@@ -249,7 +252,6 @@ print_time_tag ( const time_t           t,
   print_string_tag("time",buf,fp,spaces);
 }
 
-
 static void
 print_bounds_tag ( const position_type * sw,
                    const position_type * ne,
@@ -261,7 +263,6 @@ print_bounds_tag ( const position_type * sw,
       SEMI2DEG(sw->lat), SEMI2DEG(sw->lon),
       SEMI2DEG(ne->lat), SEMI2DEG(ne->lon));
 }
-
 
 static void
 print_route_points ( route_point * points,
@@ -276,29 +277,29 @@ print_route_points ( route_point * points,
     fprintf(fp, "<ele>%f</ele>\n", rp->elev);
     print_time_tag(rp->t + TIME_OFFSET, fp, spaces+2);
     if (rp->lap) {
-        print_spaces(fp, spaces+2);
-    	fprintf(fp,"<name>Lap %i</name>\n",rp->lap);
+      print_spaces(fp, spaces+2);
+      fprintf(fp,"<name>Lap %i</name>\n",rp->lap);
     } else if (rp->pause) {
-        print_spaces(fp, spaces+2);
-    	fprintf(fp,"<name>Pause</name>\n");
+      print_spaces(fp, spaces+2);
+      fprintf(fp,"<name>Pause</name>\n");
     }
 
-    if ((rp->hr !=0xff) || (rp->cad != 0xff)) {
-		print_spaces(fp, spaces+2);
-		fprintf(fp,"<extensions>\n");
-		print_spaces(fp, spaces+2);
-		fprintf(fp,"<gpxtpx:TrackPointExtension>\n");
-		if (rp->hr != 0xff) {
-			print_spaces(fp, spaces+4);
-			fprintf(fp,"<gpxtpx:hr>%i</gpxtpx:hr>\n",rp->hr);
-		}
-		if (rp->cad != 0xff) {
-			print_spaces(fp, spaces+4);
-			fprintf(fp,"<gpxtpx:cad>%i</gpxtpx:cad>\n",rp->cad);
-		}
+    if ((rp->hr != 0) || (rp->cad != 0xff)) {
+      print_spaces(fp, spaces+2);
+      fprintf(fp,"<extensions>\n");
+      print_spaces(fp, spaces+2);
+      fprintf(fp,"<gpxtpx:TrackPointExtension>\n");
+      if (rp->hr != 0) {
+	print_spaces(fp, spaces+4);
+	fprintf(fp,"<gpxtpx:hr>%i</gpxtpx:hr>\n",rp->hr);
+      }
+      if (rp->cad != 0xff) {
+	print_spaces(fp, spaces+4);
+	fprintf(fp,"<gpxtpx:cad>%i</gpxtpx:cad>\n",rp->cad);
+      }
 
-		print_close_tag("gpxtpx:TrackPointExtension", fp, spaces+2);
-		print_close_tag("extensions", fp, spaces+2);
+      print_close_tag("gpxtpx:TrackPointExtension", fp, spaces+2);
+      print_close_tag("extensions", fp, spaces+2);
     }
 
     print_close_tag("trkpt", fp, spaces);
@@ -315,24 +316,23 @@ print_gpx_data ( garmin_data * data, FILE * fp, int spaces )
   position_type  ne;
 
   if ( get_gpx_data(data,&laps,&sw,&ne) != 0 ) {
-	    print_gpx_header(fp,spaces);
-	    print_time_tag(time(NULL),fp,spaces);
-	    print_bounds_tag(&sw,&ne,fp,spaces);
+    print_gpx_header(fp,spaces);
+    print_time_tag(time(NULL),fp,spaces);
+    print_bounds_tag(&sw,&ne,fp,spaces);
 
-	    int i;
-		print_open_tag("trk",fp,spaces);
-	  for (i=0; laps[i]!=NULL; i++) {
-		  points=laps[i];
-			print_open_tag("trkseg",fp,spaces);
-		print_route_points(points,fp,spaces+2);
-		print_close_tag("trkseg",fp,spaces);
-	  }
-		print_close_tag("trk",fp,spaces);
-	  print_close_tag("gpx",fp,spaces);
+    int i;
+    print_open_tag("trk",fp,spaces);
+    for (i=0; laps[i]!=NULL; i++) {
+      points=laps[i];
+      print_open_tag("trkseg",fp,spaces);
+      print_route_points(points,fp,spaces+2);
+      print_close_tag("trkseg",fp,spaces);
+    }
+    print_close_tag("trk",fp,spaces);
+    print_close_tag("gpx",fp,spaces);
 
-	  free(laps[i]);
-	  free(laps);
-
+    free(laps[i]);
+    free(laps);
   }
 }
 
@@ -342,13 +342,13 @@ main ( int argc, char ** argv )
 {
   garmin_data * data;
   int           i;
-
+  
   for ( i = 1; i < argc; i++ ) {
     if ( (data = garmin_load(argv[i])) != NULL ) {
       print_gpx_data(data,stdout,0);
       garmin_free_data(data);
     }
   }
-
+  
   return 0;
 }
